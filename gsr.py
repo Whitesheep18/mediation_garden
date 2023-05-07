d1cf88e3b2d1708e699fd5157a5dc9a0cd3f0251
Anna Reisz <reiszanna08@gmail.com>
2023-05-07 21:01:27 +0200

    galvanic skin response summary

---
+++ gsr.py
@@ -0,0 +1,27 @@
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from bvp_to_hr import read_data
+
+def get_gsr_summary(df):
+    summary = df[['Phase', 'GSR']].dropna().groupby("Phase").mean()
+    summary['diff_from_baseline'] = np.concatenate([np.zeros(1), summary['GSR'].values[1:] - summary['GSR'].values[:-1]])
+    summary = summary.rename(columns = {'GSR': 'mean_GSR'})
+    return summary
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print('Wrong usage; try $ python gsr.py <path_to_file>')
+        exit()
+
+    print('reading file...')
+    path_to_file = sys.argv[1]
+    df = read_data(path_to_file)
+
+    print("processing file...")
+    summary = get_gsr_summary(df)
+    
+    print("writing file...")
+    filename, ext = path_to_file.split('.')
+    summary.to_csv(f"{filename}_summary_gsr.{ext}")
